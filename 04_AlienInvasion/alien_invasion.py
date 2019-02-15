1202316416c0e8c6c01a81fd126fffcd74b13e50
lxxxc <lxc115@126.com>
2019-02-15 22:13:17 +0800

    Update

--- 04_AlienInvasion/alien_invasion.py
+++ 04_AlienInvasion/alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 import game_functions as gf
 from settings import Settings
@@ -12,13 +13,16 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(ai_settings, screen)
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
 
     bg_color = (230, 230, 230)
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
